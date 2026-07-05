22c14745a4fba4f61e97f1b6cdec93271b72e66f
DHua5922 <hua.dylan@gmail.com>
2026-07-04 21:50:05 -0500

    create home page

--- js/api.js
+++ js/api.js
@@ -1,6 +1,14 @@
 const apiBaseUrl = "https://api.themoviedb.org";
 const publicApiKey = "e1dc213ccbcaa6eadb77075160fe39dd";
 
+async function getTrendingMoviesApi() {
+  const response = await fetch(
+    `${apiBaseUrl}/3/trending/movie/week?api_key=${publicApiKey}&language=en-US`,
+  );
+  const data = await response.json();
+  return data.results;
+}
+
 async function searchMoviesApi(queryValue, page = 1) {
   const response = await fetch(
     `${apiBaseUrl}/3/search/movie?api_key=${publicApiKey}&language=en-US&query=${queryValue}&page=${page}`,
--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-const moviesWrapper = document.querySelector(".movies__list");
 const movieSearchInput = document.querySelector(".header__movie-search-input");
 
 const moviePageForm = document.querySelector(".movies__pagination-form");
@@ -8,58 +7,15 @@ let maxPages = 1;
 let movies = null;
 
 window.addEventListener("DOMContentLoaded", () => {
-  const filterSelect = document.querySelector(".movies__select");
-  filterSelect.addEventListener("change", filterMovies);
-
-  const form = document.querySelector(".header__movie-search-form");
-  form.addEventListener("submit", searchForMovies);
-
-  moviePageForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const page = Number(moviePageInput.value);
-
-    renderLoading();
-    const data = await searchMoviesApi(movieSearchInput.value, page);
-    movies = data.results;
-    renderMovies();
-  });
-  moviePageInput.addEventListener("input", (event) => {
-    const newPageNum = Number(event.target.value);
-    event.target.value = Math.max(1, Math.min(maxPages, newPageNum));
-  });
+  loadTrendingMovies();
 });
 
-async function searchForMovies(event) {
-  event.preventDefault();
-
-  renderLoading();
-  const data = await searchMoviesApi(movieSearchInput.value);
-  movies = data.results;
-  maxPages = data.total_pages;
-  renderMovies();
-
-  document.querySelector(".movies__results--label").innerText =
-    `Search Results: ${data.total_results} movies found`;
-
-  moviePageForm.style.display = "flex";
-  moviePageInput.value = 1;
-  document.querySelector(".movies__total-pages").textContent = maxPages;
-}
-
-function filterMovies(event) {
-  const filter = event.target.value;
-
-  if (filter === "alphabetical") {
-    movies.sort((a, b) => a.title.localeCompare(b.title));
-  } else if (filter === "reverse-alphabetical") {
-    movies.sort((a, b) => b.title.localeCompare(a.title));
-  } else if (filter === "newest") {
-    movies.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
-  } else if (filter === "oldest") {
-    movies.sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
-  }
-
-  renderMovies();
+async function loadTrendingMovies() {
+  // renderLoading();
+  try {
+    const list = await getTrendingMoviesApi();
+    renderMovies(list);
+  } catch (err) {}
 }
 
 function renderLoading() {
@@ -77,19 +33,16 @@ function renderLoading() {
     .join("");
 }
 
-function renderMovies() {
+function renderMovies(list) {
+  const moviesWrapper = document.querySelector(".carousel");
+
   moviesWrapper.innerHTML = "";
-  movies.forEach((movie) => {
-    const containerElem = document.createElement("li");
-    containerElem.classList.add("movie__wrapper");
-
-    const linkElem = document.createElement("a");
-    linkElem.classList.add("movie__img-link");
-    linkElem.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500/${movie.backdrop_path}')`;
-    linkElem.innerHTML = `<h3 class="text-dark movie__title">${movie.title}</h3>`;
-    linkElem.href = `movie.html?id=${movie.id}`;
-
-    containerElem.appendChild(linkElem);
+  list.forEach((movie) => {
+    const containerElem = document.createElement("a");
+    containerElem.classList.add("carousel-item", "relative");
+    containerElem.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500/${movie.backdrop_path}')`;
+    containerElem.href = `movie.html?id=${movie.id}`;
+    containerElem.innerHTML = `<h3 class="text-dark movie__title">${movie.title}</h3>`;
     moviesWrapper.appendChild(containerElem);
   });
 }
